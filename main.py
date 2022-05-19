5cad2ddfe711ba94ed8cb1666881d179d375a885
divyam29 <divyamjain29@gmail.com>
2022-05-19 21:10:53 +0530

    Modify

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+from distutils.command.upload import upload
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import os
 from werkzeug.utils import secure_filename
-from flask_mail import Mail, Message
+from flask_mail import Mail
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from google_drive_downloader import GoogleDriveDownloader as gdd
 
 app = Flask(__name__)
 app.secret_key = 'super-secret-key'
@@ -22,11 +26,15 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] =\
     'sqlite:///' + os.path.join(basedir, 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['UPLOAD_FOLDER'] = 'D:/Projects/open cv/Rana Sir project/static/uploads'
+
+upload_dir=os.path.join(os.getcwd(),'static/uploads').replace(os.sep,'/')
+app.config['UPLOAD_FOLDER'] = upload_dir
 
 db = SQLAlchemy(app)
-
 mail = Mail(app)
+
+gauth = GoogleAuth()
+drive = GoogleDrive(gauth)
 
 
 class File(db.Model):
@@ -41,6 +49,7 @@ def home():
     if request.method == 'POST':
         file = request.files['file']
         filename = secure_filename(file.filename)
+        print(filename)
         file.save(os.path.join(
             app.config['UPLOAD_FOLDER'], filename
         ))
@@ -53,11 +62,28 @@ def home():
         db.session.add(entry)
         db.session.commit()
 
-        msg = Message(subject=filename, sender="divyamjain2907@gmail.com",
-                      recipients=[email], body="Your file has been uploaded successfully\nFile Size: " + str(filesize/1025.14) + " KBs")
-        with app.open_resource("static/uploads/" + filename) as fp:
-            msg.attach(filename, "image/png", fp.read())
-            mail.send(msg)
+        # msg = Message(subject=filename, sender="aaabb29072002@gmail.com",
+        #               recipients=[email], body="Your file has been uploaded successfully\nFile Size: " + str(filesize/1025.14) + " KBs")
+        # with app.open_resource("static/uploads/" + filename) as fp:
+        #     msg.attach(filename, "image/png", fp.read())
+        #     mail.send(msg)
+
+        upload_file_list = [filename]
+        for upload_file in upload_file_list:
+            gfile = drive.CreateFile(
+                {'parents': [{'id': '1gB6cfJFNvWwS9CT3_c4KLpDIjvkU8g2u'}]})
+            # Read file and set it as the content of this instance.
+            gfile.SetContentFile("static/uploads/" + upload_file)
+            gfile.Upload()  # Upload the file.
+
+        file_list = drive.ListFile({'q': "'{}' in parents and trashed=false".format(
+            '1gB6cfJFNvWwS9CT3_c4KLpDIjvkU8g2u')}).GetList()
+        for file in file_list:
+            if file['title'] == "static/uploads/"+filename:
+                file_id = file['id']
+                print(file['title']+" : "+file['id'])
+
+            print('title: %s, id: %s' % (file['title'], file['id']))
 
     return render_template("index.html")
 
